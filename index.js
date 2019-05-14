2c1c54606e0f19b13e5079df3a60ecdd7f33a07b
RBENT\mmao <jedny091@gmail.com>
2019-05-14 11:55:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const absolutePath = path.join(__dirname, './static')
+app.use(express.static(absolutePath));
+
+app.get('/:id(\\d+)/', function (req, res){
+  res.sendFile(path.join(absolutePath, "/index.html"));
+});
+
+app.listen(300, () => {
+  console.log('Sever is up on port 300.');
+});
+
