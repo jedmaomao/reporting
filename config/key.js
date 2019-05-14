97f6a60bbd9dc6982fe2bd1ff41d7602cad4de78
RBENT\mmao <jedny091@gmail.com>
2019-05-14 13:08:27 -0400

    update syle

---
+++ config/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: 'reportingaurora.ct0vtd3stxoq.us-east-1.rds.amazonaws.com',
+    user     : 'mmao',
+    password : 'welcome.313',
+    port:3306,
+    database: 'Reporting'
+};
