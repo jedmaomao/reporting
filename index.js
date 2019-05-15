f5b7d800bc7f0ab7304b12d35008027ac0ad27db
RBENT\mmao <jedny091@gmail.com>
2019-05-15 15:11:26 -0400

    update

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const sql = require('./services/sql');
 
 const absolutePath = path.join(__dirname, './static')
 app.use(express.static(absolutePath));
@@ -10,14 +9,8 @@ app.get('/:id(\\d+)/', function (req, res) {
   res.sendFile(path.join(absolutePath, "/index.html"));
 });
 
-app.get('/:libraryId/circulations/:option', function (req, res) {
-  const query = req.params.option == 1 ? `SELECT LibraryID, DATE_FORMAT(DATE,'%m/%d') as DATE, Service, SUM(Transactions) AS Transactions FROM Circulations WHERE Date BETWEEN NOW() - INTERVAL 30 DAY AND NOW() AND LibraryID = ${req.params.libraryId} group BY Date, Service ORDER BY DATE DESC` : `SELECT  LibraryID, DATE_FORMAT(DATE,'%M, %y') as Month, Service, SUM(Transactions) AS Transactions from Circulations where DATE > DATE_SUB(now(), INTERVAL 12 MONTH) AND libraryID = ${req.params.libraryId} GROUP BY DATE_FORMAT(DATE,'%Y-%m') DESC, Service`;
-  sql.run(query).then(data => {
-    res.send(data);
-  }).catch(err => {
-    throw new Error(err);
-  }); 
-});
+require('./routes/circulationsRoute')(app);
+
 
 app.listen(3000, () => {
   console.log('Sever is up on port 3000.');
