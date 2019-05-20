e54e57681dffd99c83a8e4a7a29d0c9903e0988e
RBENT\mmao <jedny091@gmail.com>
2019-05-20 01:08:15 -0400

    update charts

--- routes/patronCirculations.js
+++ routes/patronCirculations.js
@@ -6,7 +6,7 @@ module.exports = app => {
       let query = "";
       switch(req.params.option) {
         case 'active': query = `SELECT  libraryId, month, year, sum(ActivePatrons) as count FROM ActivePatrons WHERE (MONTH > month(NOW()) AND YEAR = YEAR(NOW()) - 1 OR YEAR = YEAR(NOW())) and libraryID = ${req.params.libraryId} GROUP BY YEAR, MONTH ORDER BY YEAR DESC, MONTH DESC`; break;
-        case 'new': query = `SELECT libraryId, month, year, SUM(PatronCount) as count FROM NewPatrons WHERE (MONTH > month(NOW()) AND YEAR = YEAR(NOW()) - 1 OR YEAR = YEAR(NOW())) and libraryID = 1062 GROUP BY YEAR, MONTH ORDER BY YEAR DESC, MONTH DESC`; break;
+        case 'new': query = `SELECT libraryId, month, year, SUM(PatronCount) as count FROM NewPatrons WHERE (MONTH > month(NOW()) AND YEAR = YEAR(NOW()) - 1 OR YEAR = YEAR(NOW())) and libraryID = ${req.params.libraryId} GROUP BY YEAR, MONTH ORDER BY YEAR DESC, MONTH DESC`; break;
       }
       sql.run(query).then(data => {
         res.send(data);
